33073bd3f4ad013e4157156398e65d5346fcdcdd
Mike Covington <mfcovington@gmail.com>
2014-01-04 10:06:58 -0800

    Outline script

---
+++ extract-seq-flanking-read.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+# Mike Covington
+# created: 2014-01-04
+#
+# Description:
+#
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+
+get_read_info();
+extract_flanking_seqs();
+
+exit;
+
+sub get_read_info {
+    # body...
+}
+
+sub extract_flanking_seqs {
+    # body...
+}
